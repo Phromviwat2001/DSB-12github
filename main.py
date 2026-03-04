76a59c27cceb4dbc32af369b796894a80aade1bc
Pu <purock999@gmail.com>
2026-03-04 08:00:27 +0000

    add add_two function

--- main.py
+++ main.py
@@ -7,3 +7,9 @@ print("I'm a good data analyst")
 ## list
 mom_list = ["milk", "egg", "water", "banana", "rice"]
 len(mom_list)
+
+## new function
+def add_two(num):
+   return num + 2
+
+print(add_two(2), add_two(5))
