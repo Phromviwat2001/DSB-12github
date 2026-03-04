95d1e75fafd434a5dd4f2aaf5105efa32dc1750f
Pu <purock999@gmail.com>
2026-03-04 15:44:40 +0700

    Now i know what a githubmain.py

--- main.py
+++ main.py
@@ -1,15 +1,2 @@
 ## print("Hello world)
-print("Hello world")
-
-## print key for data analyst
-print("I'm a good data analyst")
-
-## list
-mom_list = ["milk", "egg", "water", "banana", "rice"]
-len(mom_list)
-
-## new function
-def add_two(num):
-   return num + 2
-
-print(add_two(2), add_two(5))
+print("Now i know waht a github")
